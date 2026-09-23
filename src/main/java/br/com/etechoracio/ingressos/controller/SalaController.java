209f4726b1af7cbfb00c4dc669a008ddd16eee01
Dederious <derick8salvadorsantos@gmail.com>
2026-09-23 15:27:03 -0300

    feat: etapa 2 - consulta de sala ativa por id (GET /salas/{id})

--- src/main/java/br/com/etechoracio/ingressos/controller/SalaController.java
+++ src/main/java/br/com/etechoracio/ingressos/controller/SalaController.java
@@ -1,16 +1,13 @@
 package br.com.etechoracio.ingressos.controller;
 
-import br.com.etechoracio.ingressos.entity.Filme;
 import br.com.etechoracio.ingressos.entity.Sala;
-import br.com.etechoracio.ingressos.repository.FilmeRepository;
 import br.com.etechoracio.ingressos.repository.SalaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+
 @RestController
 @RequestMapping("/salas")
 @CrossOrigin("*")
@@ -23,5 +20,12 @@ public class SalaController {
         return salaRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Sala> buscarPorId(@PathVariable Long id){
+        var sala = salaRepository.findById(id);
+        if(sala.isPresent())
+            return ResponseEntity.ok(sala.get());
+        return ResponseEntity.notFound().build();
+    }
 }
 
