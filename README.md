98f5f08ecaeb6f1b606025d9754b9f779be5d089
Dederious <derick8salvadorsantos@gmail.com>
2026-09-23 15:21:47 -0300

    feat: etapa 1 - listagem de todas as salas ativas (GET /salas)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # NOMES
+#Derick & Maria
 
---
+++ src/main/java/br/com/etechoracio/ingressos/repository/SalaRepository.java
@@ -0,0 +1,9 @@
+package br.com.etechoracio.ingressos.repository;
+
+import br.com.etechoracio.ingressos.entity.Sala;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SalaRepository extends JpaRepository <Sala, Long> {
+}
